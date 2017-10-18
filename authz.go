ca22f48c56bd172a7b93def73cc3861a3263c4f5
Prasanna Gautam <prasannagautam@gmail.com>
2017-10-18 10:32:09 -0700

    Adding a very simple Authz module

---
+++ authz.go
@@ -0,0 +1,106 @@
+package accord
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// Authz is a bare minimum Authorization interface that just checks
+// if any of the requested principals are actually valid for the user
+// The service should only grant the access to the principals the user
+// should have access to. If there is no overlap with the existing principals
+// the result is empty and with an error for more details for the service to
+// log for administrators or to send to the users
+// Any other authorization backends can be added by implementing this interface
+type Authz interface {
+	Authorized(user string, principals []string) ([]string, error)
+}
+
+// GrantAll is the authz module that grants everyone everything
+type GrantAll struct{}
+
+func (g *GrantAll) Authorized(user string, principals []string) ([]string, error) {
+	return principals, nil
+}
+
+type SimpleAuth struct {
+	Principals []string `json:"principals",yaml:"principals"`
+	// these users can get the root-everywhere if they request for it
+	// allowing to sign in with full sudo
+	AdminUsers []string `json:"admin_users",yaml:"admin_users"`
+
+	// Users -> Principals map
+	AccessMap map[string][]string `json:"access_map",yaml:"access_map"`
+}
+
+func NewSimpleAuthFromFile(filePath string) (*SimpleAuth, error){
+	s := &SimpleAuth{}
+
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Cannot read file %s", filePath)
+	}
+	err = json.Unmarshal(content, s)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to parse %s", filePath)
+	}
+	return s, nil
+}
+
+func (s *SimpleAuth) IsAdmin(user string) bool {
+	for _, adminUser := range s.AdminUsers {
+		if user == adminUser {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *SimpleAuth) validPrincipals(principals string) (bool, error) {
+	// Yes this is O(n^2) but the data size is going to be very small
+	// at most 100 elements, or at least if this is becoming a bottleneck
+	// you want something like LDAP and not hacking this simple implementation
+	for _, p := range principals {
+		if !contains(p, s.Principals) {
+			return false, fmt.Errorf("Principal %s is unknown", p)
+		}
+	}
+	return true, nil
+}
+
+func contains (needle string, haystack []string) bool {
+	for _, thread := range  {
+		if needle == thread {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *SimpleAuth) Authorized(user string, principals []string) ([]string, error) {
+	if _, err := s.validPrincipals(principals); err != nil {
+		return nil, errors.Wrapf(err, "Invalid principals")
+	}
+
+	if s.IsAdmin(user) {
+		// if admin, grant the principals if they exist
+		// allow the user access to their own directory if they have one on the server
+		return append(principals, user), nil
+	}
+
+	grantedAccess, ok := s.AccessMap[user]
+	if !ok {
+		return nil, fmt.Errorf("User not granted any access yet, talk to your administrator")
+	}
+
+	// grant user their own principal
+	grantedPrincipals := []string{}
+
+	for _, p := range principals {
+		if contains(p, grantedAccess){
+			grantedPrincipals = append(grantedPrincipals, p)
+		}
+	}
+	return grantedPrincipals, nil
+}
